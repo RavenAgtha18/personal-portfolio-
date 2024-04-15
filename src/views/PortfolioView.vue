6e1de8bace7ed044469ebd028b5ba3bb62de2ef7
riki <rikiandialfiyanto29@gmail.com>
2024-04-15 17:39:25 +0700

    menyesuikan bagian portofolio

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -24,12 +24,12 @@ export default {
         },
         {
           id: 3,
-          name: 'Library Management Apps',
-          imageUrl: 'library',
-          status: 'A web-based application with an integrated library management system that includes books, stock, catalog, members, publishers, authors, and loan transactions.',
-          tech: 'Laravel 9, Bootstrap, VueJS 3',
-          github: 'https://github.com/RavenAgtha18/perpustakaaan.gi',
-          demo: 'null'
+          name: 'Resto',
+          imageUrl: 'resto',
+          status: 'A Showcase of Culinary Excellence with Bootstrap-Powered Landing Page. Immerse yourself in the world of cutting-edge web technology as we present our culinary haven. Crafted using the Bootstrap framework, our landing page seamlessly blends elegant design and responsive functionality.',
+          tech: 'HTML, Bootstrap',
+          github: 'https://github.com/RavenAgtha18/lading-page-resto.git',
+          demo: 'https://majestic-khapse-51488b.netlify.app/'
         },
         {
           id: 4,
@@ -42,12 +42,12 @@ export default {
         },
         {
           id: 5,
-          name: 'Resto',
-          imageUrl: 'resto',
-          status: 'A Showcase of Culinary Excellence with Bootstrap-Powered Landing Page. Immerse yourself in the world of cutting-edge web technology as we present our culinary haven. Crafted using the Bootstrap framework, our landing page seamlessly blends elegant design and responsive functionality.',
-          tech: 'HTML, Bootstrap',
-          github: 'https://github.com/RavenAgtha18/lading-page-resto.git',
-          demo: 'https://majestic-khapse-51488b.netlify.app/'
+          name: 'Library Management Apps',
+          imageUrl: 'library',
+          status: 'A web-based application with an integrated library management system that includes books, stock, catalog, members, publishers, authors, and loan transactions.',
+          tech: 'Laravel 9, Bootstrap, VueJS 3',
+          github: 'https://github.com/RavenAgtha18/perpustakaaan.gi',
+          demo: 'null'
         },
       ]
     };
@@ -68,73 +68,84 @@ export default {
       </header>
       <section>
         <div>
-          <div class="grid grid-cols-1 gap-4 pb-32 md:grid-cols-3 md:gap-3 xl:grid-cols-3 xl:gap-3 2xl:gap-5 fade-zoom-in">
+          <div
+            class="grid grid-cols-1 gap-4 pb-32 md:grid-cols-3 md:gap-3 xl:grid-cols-3 xl:gap-3 2xl:gap-5 fade-zoom-in">
             <div v-for="item in items" :key="item.id">
               <div
                 class="item-card flex flex-col items-center gap-2 rounded bg-[#1e1e1f] hover:bg-[#282828] border border-[#383838] rounded-xl text-amber-50 md:gap-3 px-5 py-5 lg:px-5 ">
                 <div class="flex h-12 w-12 items-center justify-center p-0 h-full w-full lg:p-0 zoom-in">
-                  <img alt="HTML" loading="lazy" decoding="async" data-nimg="1" class="drop-shadow-xl rounded rounded-xl"
-                    :src="'/img/portfolio-' + item.imageUrl + '.png'">
+                  <img alt="HTML" loading="lazy" decoding="async" data-nimg="1"
+                    class="drop-shadow-xl rounded rounded-xl" :src="'/img/portfolio-' + item.imageUrl + '.png'">
                 </div>
                 <div class="w-full flex flex-col gap-2 items-center text-sm md:text-base lg:text-lg">
                   <div class="title-text font-medium text-secondary">{{ item.name }}
                   </div>
-                  <div class="w-full text-left text-[10px] text-[#c1c1c1] md:text-xs lg:text-sm">
-                    {{ item.status }}</div>
+                  <div class="w-full text-left text-[10px] text-[#c1c1c1] md:text-xs lg:text-sm text-status">
+                    {{ item.status }}
+                  </div>
                   <div class="w-full mt-4 text-normal text-sm text-left text-amber-200">
                     {{ item.tech }}
                   </div>
                   <div class="w-full flex justify-end">
                     <div class="flex cursor-pointer items-end gap-2 text-primary">
-                      <a v-if="item.github !== 'null'"
-                        :href="item.github" target="_blank" rel="noreferrer"
+                      <a v-if="item.github !== 'null'" :href="item.github" target="_blank" rel="noreferrer"
                         title="View github repository" class="transition-all hover:text-accent">
-                        <svg stroke="currentColor"
-                          fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"
-                          height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+                        <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24"
+                          stroke-linecap="round" stroke-linejoin="round" height="16" width="16"
+                          xmlns="http://www.w3.org/2000/svg">
                           <path
                             d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
                           </path>
                         </svg></a>
-                        <a v-if="item.demo !== 'null'" :href="item.demo" target="_blank" rel="noreferrer"
+                      <a v-if="item.demo !== 'null'" :href="item.demo" target="_blank" rel="noreferrer"
                         title="View finished project" class="transition-all hover:text-accent">
-                        <svg stroke="currentColor"
-                          fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"
-                          height="18" width="18" xmlns="http://www.w3.org/2000/svg">
+                        <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24"
+                          stroke-linecap="round" stroke-linejoin="round" height="18" width="18"
+                          xmlns="http://www.w3.org/2000/svg">
                           <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
                           <polyline points="15 3 21 3 21 9"></polyline>
-                        <line x1="10" y1="14" x2="21" y2="3"></line>
-                      </svg></a></div>
+                          <line x1="10" y1="14" x2="21" y2="3"></line>
+                        </svg></a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  </article>
-</div></template>
+      </section>
+    </article>
+  </div>
+</template>
 
 <style>
 .item-card:hover {
   transition: transform 0.3s ease;
   transform: translateY(-8px);
+
 }
-svg:hover{
+
+.text-status {
+  max-height: 100px;
+  overflow-y: auto;
+}
+
+svg:hover {
   stroke: #ffdb70;
 }
+
 @keyframes fadeZoomIn {
   0% {
     opacity: 0;
     transform: scale(0.5);
   }
+
   100% {
     opacity: 1;
     transform: scale(1);
   }
 }
 
-/* Menggunakan animasi pada elemen yang diinginkan */
 .fade-zoom-in {
   animation: fadeZoomIn 1s ease-in-out;
 }
